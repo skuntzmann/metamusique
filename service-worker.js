71b1f7b925f33602724a72473831a858fc0fc8d6
Sylvain Kuntzmann <13257444+skuntzmann@users.noreply.github.com>
2025-07-02 11:01:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,2 @@
-self.addEventListener('install', e => {
-  console.log('[SW] Install');
-  e.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', e => {
-  console.log('[SW] Activate');
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('fetch', event => {});
